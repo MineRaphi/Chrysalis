0e54d7d38546bf8fe323b9e254cab8fe167818ff
MineRaphi <MineRaphiHD@gmail.com>
2026-09-04 23:50:18 +0200

    Updated movement and animation code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,42 +1,73 @@
 extends CharacterBody2D
 
+enum MovementState { IDLE, RUN, SPRINT, JUMP, FALL, DASH }
+enum ActionState { NONE, ATTACK, HURT }
+
+var movement_state: MovementState = MovementState.IDLE
+var action_state: ActionState = ActionState.NONE
+var is_jumping = false
 
 const SPEED = 300.0
+const SPRINT_SPEED = 420.0
 const JUMP_VELOCITY = -400.0
 
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var jump = false
-
 func _physics_process(delta: float) -> void:
+	# apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if velocity.y == 0 or is_on_floor():
-		jump = false
+	if velocity.y >= 0 or is_on_floor():
+		is_jumping = false
 	
+	# jumps
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		jump = true
-		
-	if Input.is_action_just_released("jump") and jump:
+		is_jumping = true
+	
+	# jump release
+	if Input.is_action_just_released("jump") and is_jumping:
 		velocity.y = 0
-		jump = false
+		is_jumping = false
 	
+	# moves horizontaly
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = direction * SPEED
 	
-	_handle_animations()
+	_update_movement_state()
+	_update_animation()
 	move_and_slide()
 
+func _update_movement_state() -> void:
+	if not is_on_floor():
+		if velocity.y < 0:
+			movement_state = MovementState.JUMP
+		else:
+			movement_state = MovementState.FALL
+	elif abs(velocity.x) > 0.1:
+		movement_state = MovementState.RUN
+	else:
+		movement_state = MovementState.IDLE
 
-func _handle_animations() -> void:
+func _update_animation() -> void:
 	if velocity.x > 0:
 		anim_sprite.flip_h = false
 	elif velocity.x < 0:
 		anim_sprite.flip_h = true
+		
+	if action_state == ActionState.HURT:
+		print("hurt") #animation still needed
+		return
 	
-	
+	match movement_state:
+		MovementState.IDLE:
+			anim_sprite.play("idle")
+		MovementState.RUN:
+			anim_sprite.play("run")
+		MovementState.JUMP:
+			anim_sprite.play("jump")
+		MovementState.FALL:
+			anim_sprite.play("fall")
+		MovementState.DASH:
+			anim_sprite.play("dash")
