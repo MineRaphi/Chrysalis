40f4fd3b9aac6027ca382671ff3e6822e77f34fd
MineRaphi <MineRaphiHD@gmail.com>
2026-09-08 03:09:05 +0200

    Added code to modular level loader

--- Scripts/level_loader.gd
+++ Scripts/level_loader.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
-@export var level: PackedScene
+@export var level_path: String
 @export var spawn_name: String
 
+var level: PackedScene
+
 func _ready() -> void:
-	assert(level, "Level needs to be set")
+	assert(level_path != "", "Level path needs to be set")
 	assert(spawn_name != "", "Spawn name needs to be set")
 	
+	level = load(level_path)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_node("/root/Main").call_deferred("switch_to_room", level, spawn_name)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,9 +1,37 @@
 extends Node2D
 
+@export var start_room = load("res://Levels/TestLevels/test_level.tscn")
+
+@onready var current_room: Node2D = $currentRoom
+@onready var player: CharacterBody2D = $Player
+
 func _ready() -> void:
-	var start_room = load("res://Levels/TestLevels/test_level.tscn")
-	add_child(start_room.instantiate())
+	var col_lay_save = player.collision_layer
+	player.collision_layer = 0
+	
+	switch_to_room(start_room)
+	
+	await get_tree().create_timer(0.1).timeout
+	
+	player.collision_layer = col_lay_save
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func switch_to_room(level: PackedScene, spawn_name: String = "default"):
+	print(level, spawn_name)
+	var next_room = level.instantiate()
+	
+	var spawn = next_room.find_child(spawn_name)
+	
+	if spawn:
+		player.position = spawn.position
+	else:
+		player.position = Vector2(0, 0)
+	
+	for child in current_room.get_children():
+		child.queue_free()
+	
+	current_room.add_child(next_room)
