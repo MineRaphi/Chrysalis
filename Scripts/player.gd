b10abc7619368498b8886460139660a864d2c3ed
MineRaphi <MineRaphiHD@gmail.com>
2026-09-12 18:32:18 +0200

    Implemented Attack and hit test object

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,7 +15,10 @@ var movement_disabled := false
 var is_attack_on_cooldown := false
 var combo_state := 0
 var look_direction := 1
+var look_updown := 0
 var camera_distance := Vector2(0, 0)
+
+var is_attacking := false
 
 const SPEED = 240.0
 const DASH_SPEED = 600.0
@@ -38,6 +41,8 @@ const CAMERA_OFFSET_Y = -16
 @onready var wall_jump_timer: Timer = $Timer/WallJumpTimer
 @onready var combo_timer: Timer = $Timer/ComboTimer
 @onready var attack_cooldown: Timer = $Timer/AttackCooldown
+@onready var slash: AnimatedSprite2D = $Slash
+@onready var attack_box: Area2D = $AttackBox
 
 
 func _physics_process(delta: float) -> void:
@@ -105,17 +110,23 @@ func _physics_process(delta: float) -> void:
 		
 		# attack
 		if Input.is_action_just_pressed("attack") and not is_wall_sliding and not is_attack_on_cooldown and combo_state < 3:
-			action_state = ActionState.ATTACK
-			is_attack_on_cooldown = true
-			combo_state += 1
-			attack_cooldown.start()
-			combo_timer.start()
+			#action_state = ActionState.ATTACK
+			#is_attack_on_cooldown = true
+			#combo_state += 1
+			#attack_cooldown.start()
+			#combo_timer.start()
+			_slash()
 	
 	if is_dash_on_cooldown and (is_on_floor() or is_wall_sliding) and dash_cooldown.is_stopped():
 		dash_cooldown.start()
 	
 	if not is_double_jump_avalible and (is_on_floor() or is_wall_sliding):
 		is_double_jump_avalible = true
+	
+	if is_attacking:
+		for area in attack_box.get_overlapping_areas():
+			if area.name == "hitTest":
+				area.queue_free()
 	
 	_update_data()
 	_update_camera()
@@ -132,6 +143,13 @@ func _update_data() -> void:
 			look_direction = 1
 		elif velocity.x < 0:
 			look_direction = -1
+	
+	if Input.is_action_pressed("up"):
+		look_updown = 1
+	elif Input.is_action_pressed("down"):
+		look_updown = -1
+	else:
+		look_updown = 0
 
 func _update_movement_state() -> void:
 	if is_dashing:
@@ -217,3 +235,34 @@ func snap_camera_to_target():
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if area.name == "deathzone":
 		call_deferred("_kill_player")
+
+func _slash() -> void:
+	slash.visible = true
+	is_attacking = true
+	
+	if look_updown == 0:
+		if look_direction > 0:
+			slash.flip_h = false
+			attack_box.rotation_degrees = 0
+		elif look_direction < 0:
+			slash.flip_h = true
+			attack_box.rotation_degrees = 180
+		
+		slash.rotation_degrees = 0
+		slash.position = Vector2(look_direction * 14, -26)
+	elif look_updown > 0:
+		slash.rotation_degrees = 90
+		slash.flip_h = true
+		attack_box.rotation_degrees = -90
+		slash.position = Vector2(6, -32)
+	else:
+		slash.rotation_degrees = 90
+		slash.flip_h = false
+		attack_box.rotation_degrees = 90
+		slash.position = Vector2(7, -8)
+	
+	slash.play("slash")
+	
+	await slash.animation_finished
+	slash.visible = false
+	is_attacking = false
